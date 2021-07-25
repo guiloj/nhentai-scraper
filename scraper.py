8baa4a7ed0c49bf58ce3bbe9eb1a83e796417abb
guiloj <61667899+guiloj@users.noreply.github.com>
2021-07-25 02:17:58 -0300

    polished code UwU

--- scraper.py
+++ scraper.py
@@ -1,101 +1,153 @@
-# was going to use urllib but nhentai was super rude and gave me a 403
-# from urllib import request
-# import chromedriver_binary
 import time
 import re
 from selenium import webdriver
 import os
-# web webdriver/selenium stuff
-options = webdriver.ChromeOptions()
-# # was pissing me off with errors because i had my tablet plugged in
-options.add_experimental_option('excludeSwitches', ['enable-logging'])
-browser = webdriver.Chrome(options=options)
+import sys
+os.chdir(r'C:/Users/guilo/Documents/Atom_Projects/Python/reddit')
+os.system('cls' if os.name == 'nt' else 'clear')
 
-# gets all the links from this file and puts them in a list
-with open('links.txt', 'r', encoding='utf-8') as f:
-    links = f.readlines()
+print('''
+░██████╗███████╗██╗░░░░░███████╗███╗░░██╗██╗██╗░░░██╗███╗░░░███╗
+██╔════╝██╔════╝██║░░░░░██╔════╝████╗░██║██║██║░░░██║████╗░████║
+╚█████╗░█████╗░░██║░░░░░█████╗░░██╔██╗██║██║██║░░░██║██╔████╔██║
+░╚═══██╗██╔══╝░░██║░░░░░██╔══╝░░██║╚████║██║██║░░░██║██║╚██╔╝██║
+██████╔╝███████╗███████╗███████╗██║░╚███║██║╚██████╔╝██║░╚═╝░██║
+╚═════╝░╚══════╝╚══════╝╚══════╝╚═╝░░╚══╝╚═╝░╚═════╝░╚═╝░░░░░╚═╝
 
-# gets all the numbers ever used by the bot to see if one was already used automatically
-with open('dupli.txt', 'r', encoding='utf-8') as f:
-    dupli = []
-    f = f.readlines()
-    for x in f:
-        dupli.append(x.replace('\n',''))
+              █   █▀█ ▄▀█ █▀▄ █ █▄ █ █▀▀ 
+              █▄▄ █▄█ █▀█ █▄▀ █ █ ▀█ █▄█ ▄ ▄ ▄''')
 
-
-for link in links:
-    # checks if the list element is a number or a link than gets the link and number of each one
+def get_tags(link, current, total):
+    print('\033[K', end='\r')
+    print(f'>>[?][{current}/{total}] parsing link', end='\r')
     if len(link) < 6:
         url = 'https://nhentai.net/g/'+link+'/'
         number = link
     else:
-        url = link
-        number = re.search("([0-9]+)", link).group(0)
-    # checks if the number is in dupli.txt or not, if so warn the user and go back to the start of the for with the next elemnt
+        url = link.replace('\n', '')
+        number = re.search("([0-9]{1,6})", link).group(0)
     if number in dupli:
-        print(number, 'duplicated')
-        continue
-    # opens url (takes ages the first time)
+        print('\033[K', end='\r')
+        print(f'>>[{number}][{current}/{total}] {number} is duplicaded', end='\r')
+        return
+    print('\033[K', end='\r')
+    print(f'>>[{current}/{total}] opening ({url})...', end='\r')
     browser.get(url)
-    # finds the element we are interested in
+    print('\033[K', end='\r')
+    print(f'>>[{number}][{current}/{total}] {url} opened', end='\r')
     search = browser.find_element_by_id('tags')
-    # hippity hoppity that html is now my property
     html = search.get_attribute('innerHTML')
-    # ladies and gentleman, I present to you, the worse code I have ever written in my whole life
-    elem = html.split('\n')
-    parodies = []; characters = []; tags = []; artists = []; groups = []; languages = []
-    par = "class=\"name\"\>([- A-Za-z0-9_|]+)\<" # this took me way more time than it should
-    for x in range(len(elem)):
-        if 'Parodies:' in elem[x]:
-            parodies = re.findall(par, elem[x+1])
-        elif 'Characters:' in elem[x]:
-            characters = re.findall(par, elem[x+1])
-        elif 'Tags:' in elem[x]:
-            tags = re.findall(par, elem[x+1])
-        elif 'Artists:' in elem[x]:
-            artists = re.findall(par, elem[x+1])
-        elif 'Groups:' in elem[x]:
-            groups = re.findall(par, elem[x+1])
-        elif 'Languages:' in elem[x]:
-            languages = re.findall(par, elem[x+1])
+    info = html.split('\n')
+    tags = {
+        'parodies':[],
+        'characters':[],
+        'tags':[],
+        'artists':[],
+        'groups':[],
+        'languages':[]
+    }    
+    print('\033[K', end='\r')
+    print(f'>>[{number}][{current}/{total}] finding tags', end='\r')
+    par = r"class=\"name\"\>([- A-Za-z0-9_|]+)\<"
+    for x in range(len(info)):
+        if 'Parodies:' in info[x]:
+            tags['parodies'] = re.findall(par, info[x+1])
+        elif 'Characters:' in info[x]:
+            tags['characters'] = re.findall(par, info[x+1])
+        elif 'Tags:' in info[x]:
+            tags['tags'] = re.findall(par, info[x+1])
+        elif 'Artists:' in info[x]:
+            tags['artists'] = re.findall(par, info[x+1])
+        elif 'Groups:' in info[x]:
+            tags['groups'] = re.findall(par, info[x+1])
+        elif 'Languages:' in info[x]:
+            tags['languages'] = re.findall(par, info[x+1])
+    print('\033[K', end='\r')
+    print(f'>>[{number}][{current}/{total}] tags found', end='\r')
+    
+    tags['parodies'] = ' '.join(['<'+x+'>' for x in tags['parodies'][:6]])
+    tags['characters'] = ' '.join(['<'+x+'>' for x in tags['characters'][:6]])
+    tags['tags'] = ' '.join(['<'+x+'>' if x != 'multi-work ' else x.replace('multi-work', 'series') for x in tags['tags'][:6]])
+    tags['artists'] = ' '.join(['<'+x+'>' for x in tags['artists'][:6]])
+    tags['groups'] = ' '.join(['<'+x+'>' for x in tags['groups'][:6]])
+    tags['languages'] = ' '.join(['<'+x+'>' for x in tags['languages'][:6]])
 
-    # what? never heard of a dict
-    p = ''; c = ''; t = ''; a = ''; g = ''; l = ''
-    for x in parodies[:6]:
-        p += ' <{}>'.format(x)
-    for x in characters[:6]:
-        c += ' <{}>'.format(x)
-    for x in tags[:6]:
-        t += ' <{}>'.format(x.replace('multi-work ', ''))
-    for x in artists[:6]:
-        a += ' <{}>'.format(x)
-    for x in groups[:6]:
-        g += ' <{}>'.format(x)
-    for x in languages[:6]:
-        l += ' <{}>'.format(x)
-    # get the image link to make my life easier
     search = browser.find_element_by_id('cover')
     html = search.get_attribute('innerHTML')
-    # im telling you man, im afraid of beautiful soup
-    img = re.search("(?P<url>https?://[^\s]+)", html).group(0).replace('"', '')
-    # add the thing to dupli
+    img = re.search(r"(?P<url>https?://[^\s\"]+)", html).group(0)
+    print('\033[K', end='\r')
+    print(f'>>[{number}][{current}/{total}] img found', end='\r')
     with open('dupli.txt', 'a', encoding='utf-8') as f:
         f.write(number + '\n')
-    # append output
+    
+    print('\033[K', end='\r')
+    print(f'>>[{number}][{current}/{total}] number written to dupli.txt', end='\r')
     with open('output.txt', 'a', encoding='utf-8') as f:
-        f.write("Tags:{}\nParodies:{}\nCharacters:{}\nArtists:{}\nGroups:{}\nLanguages:{}\n\n#{}\n{}\n{}\n\n\n".format(t, p, c, a, g, l, number, url, img))
-    # looks like this:
-    #
-    # Tags: <tag1> <tag2> <tag3> <tag4> <tag5> <tag6> ...
-    # Parodies: <parodie> ...
-    # Characters: <character> ...
-    # Artists: <artist> ...
-    # Groups: <group> ...
-    # Languages: <language> ...
-    #
-    # #000000
-    # https://nhentai.net/g/000000/
-    #
-    # https://t.nhentai.net/galleries/0000000/cover.png
-# my processor finally gets sweet relif
+        f.write("Tags: {}\nParodies: {}\nCharacters: {}\nArtists: {}\nGroups: {}\nLanguages: {}\n\n#{}\n{}\n{}\n\n\n".format(tags['tags'], tags['parodies'], tags['characters'], tags['artists'], tags['groups'], tags['languages'], number, url, img))
+    
+    print('\033[K', end='\r')
+    print(f'>>[{number}][{current}/{total}] output written to output.txt', end='\r')
+options = webdriver.ChromeOptions()
+options.add_experimental_option('excludeSwitches', ['enable-logging'])
+browser = webdriver.Chrome(options=options)
+os.system('cls' if os.name == 'nt' else 'clear')
+print('''
+                        ████████╗██╗░░██╗███████╗  
+                        ╚══██╔══╝██║░░██║██╔════╝  
+                        ░░░██║░░░███████║█████╗░░  
+                        ░░░██║░░░██╔══██║██╔══╝░░  
+                        ░░░██║░░░██║░░██║███████╗  
+                        ░░░╚═╝░░░╚═╝░░╚═╝╚══════╝  
+
+███████╗██╗░░░░░░█████╗░████████╗  ░█████╗░██╗░░██╗███████╗░██████╗████████╗  
+██╔════╝██║░░░░░██╔══██╗╚══██╔══╝  ██╔══██╗██║░░██║██╔════╝██╔════╝╚══██╔══╝  
+█████╗░░██║░░░░░███████║░░░██║░░░  ██║░░╚═╝███████║█████╗░░╚█████╗░░░░██║░░░  
+██╔══╝░░██║░░░░░██╔══██║░░░██║░░░  ██║░░██╗██╔══██║██╔══╝░░░╚═══██╗░░░██║░░░  
+██║░░░░░███████╗██║░░██║░░░██║░░░  ╚█████╔╝██║░░██║███████╗██████╔╝░░░██║░░░  
+╚═╝░░░░░╚══════╝╚═╝░░╚═╝░░░╚═╝░░░  ░╚════╝░╚═╝░░╚═╝╚══════╝╚═════╝░░░░╚═╝░░░  
+
+    ███████╗███╗░░██╗░░░░░██╗░█████╗░██╗░░░██╗███████╗██████╗░██╗░██████╗  
+    ██╔════╝████╗░██║░░░░░██║██╔══██╗╚██╗░██╔╝██╔════╝██╔══██╗╚█║██╔════╝  
+    █████╗░░██╔██╗██║░░░░░██║██║░░██║░╚████╔╝░█████╗░░██████╔╝░╚╝╚█████╗░  
+    ██╔══╝░░██║╚████║██╗░░██║██║░░██║░░╚██╔╝░░██╔══╝░░██╔══██╗░░░░╚═══██╗  
+    ███████╗██║░╚███║╚█████╔╝╚█████╔╝░░░██║░░░███████╗██║░░██║░░░██████╔╝  
+    ╚══════╝╚═╝░░╚══╝░╚════╝░░╚════╝░░░░╚═╝░░░╚══════╝╚═╝░░╚═╝░░░╚═════╝░  
+
+    ██╗░░██╗░█████╗░██╗░░░░░██╗░░░██╗  ██████╗░██╗██████╗░██╗░░░░░███████╗
+    ██║░░██║██╔══██╗██║░░░░░╚██╗░██╔╝  ██╔══██╗██║██╔══██╗██║░░░░░██╔════╝
+    ███████║██║░░██║██║░░░░░░╚████╔╝░  ██████╦╝██║██████╦╝██║░░░░░█████╗░░
+    ██╔══██║██║░░██║██║░░░░░░░╚██╔╝░░  ██╔══██╗██║██╔══██╗██║░░░░░██╔══╝░░
+    ██║░░██║╚█████╔╝███████╗░░░██║░░░  ██████╦╝██║██████╦╝███████╗███████╗
+    ╚═╝░░╚═╝░╚════╝░╚══════╝░░░╚═╝░░░  ╚═════╝░╚═╝╚═════╝░╚══════╝╚══════╝''')
+browser.get('file:///C:/path/to/your/html/file/start.html')
+print('\n\n')
+print('>>opening links.txt')
+with open('links.txt', 'r', encoding='utf-8') as f:
+    links = f.readlines()
+print('>>done\n')
+
+print('>>opening dupli.txt')
+with open('dupli.txt', 'r', encoding='utf-8') as f:
+    dupli = []
+    f = f.readlines()
+    print('>>iteraring')
+    for x in f:
+        dupli.append(x.replace('\n',''))
+
+print('>>done\n')
+
+input('>>proceed(enter): ')
+
+print('\n>>entering for loop\n')
+print('\033[K', end='\r')
+print('>>starting...', end='\r')
+
+nums = [*range(1, len(links)+1)]
+for x, y in zip(links, nums):
+    get_tags(x, y, len(links))
+
+print('\n\n>>DONE')
+browser.get('file:///C:/path/to/your/html/file/end.html')
+input('>>exit(enter): ')
 browser.quit()
+sys.exit(0) 
